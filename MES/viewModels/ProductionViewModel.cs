d36b52b84ee85a4729dd063277b91cd32b0524da
zhangqingde <18762675195@126.com>
2024-04-23 14:47:31 +0800

    新增：生产计划界面

--- MES/viewModels/ProductionViewModel.cs
+++ MES/viewModels/ProductionViewModel.cs
@@ -1,10 +1,28 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace MES.viewModels
 {
     partial class ProductionViewModel : ObservableObject
     {
-        [ObservableProperty]
-        private string title = "hello world";
+        #region 私有方法
+        [RelayCommand]
+        private void AddProject()
+        {
+
+        }
+
+        [RelayCommand]
+        private void AddDispatcher()
+        {
+
+        }
+
+        [RelayCommand]
+        private void AddMonitor()
+        {
+
+        }
+        #endregion
     }
 }
